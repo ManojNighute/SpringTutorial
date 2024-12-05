328ed6b65c420ac26964098cefadebfb6f2e7c51
Manoj Nighute <41262509+ManojNighute@users.noreply.github.com>
2024-12-05 12:20:04 +0530

    Added Bean scope annotation
    Added @Scope and scope in XML

--- src/main/java/org/example/stereotype/Test.java
+++ src/main/java/org/example/stereotype/Test.java
@@ -7,9 +7,14 @@ public class Test {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("stereoconfig.xml");
         Associate associate = applicationContext.getBean("associate",Associate.class);
-        System.out.println(associate.hashCode());
+//        System.out.println(associate.hashCode());
 
         Associate associate1 = applicationContext.getBean("associate",Associate.class);
-        System.out.println(associate1.hashCode());
+//        System.out.println(associate1.hashCode());
+
+        Teacher t1 = applicationContext.getBean("teacher",Teacher.class);
+        Teacher t2 = applicationContext.getBean("teacher",Teacher.class);
+        System.out.println(t1.hashCode());
+        System.out.println(t2.hashCode());
     }
 }
